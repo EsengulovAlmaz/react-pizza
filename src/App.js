ac662efaeb012bc43cc33cb496bd8e024906f565
Almaz <almaz.esengulov.pi9.03.20@gmail.com>
2023-05-17 21:15:06 +0600

    fix styles

--- src/App.js
+++ src/App.js
@@ -9,19 +9,17 @@ import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
 
 function App() {
-  
+
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <Routes>
-            <Route index element={<Home />}/>
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />}/>
-          </Routes>
-        </div>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
