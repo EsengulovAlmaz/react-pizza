d4aec50bae6629ccf4e603b2da26b5cbc6859527
Almaz <almaz.esengulov.pi9.03.20@gmail.com>
2023-06-23 19:22:48 +0600

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Layout from './layouts/Layout';
+import FullPizza from './pages/FullPizza';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
-
-const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */"./pages/Cart"));
-const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */"./pages/FullPizza"));
-const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */"./pages/NotFound"));
 
 function App() {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,9 @@ import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    <React.Suspense fallback={<h2>Загрузка...</h2>}>
-        <BrowserRouter>
-            <Provider store={store} >
-                <App />
-            </Provider>
-        </BrowserRouter>
-    </React.Suspense>
+    <BrowserRouter>
+        <Provider store={store} >
+            <App />
+        </Provider>
+    </BrowserRouter>
 );
